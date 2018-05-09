00c3dd2dba2cfcb4f292c238e8b0d623837c8b11
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-09 15:41:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
 
+def my_collect( ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
+  collection = []
+  i=0
+   while i< collection.length
+     yield(collection[i])
+     i+=
+   end
+   collection
+ end
+
+  my_collect( ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]) do |i|
+  collection>> i.upcase
+end
