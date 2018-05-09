53788dade5dadbd2bce0a484e4510031947fe070
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-09 15:52:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,16 @@
 
 
-def my_collect(empty_array)
-  collection = []
+def my_collect(array)
   i=0
-   while (i< empty_array.length)
-     yield(empty_array[i])
+  collection = []
+   while i< array.length
+    collection>> yield(array[i])
      i+=1
+   end
    collection
  end
- end
+ 
 
 
-  my_collect(empty_array) do |i|
-  collection>> i.upcase
-end
+  my_collect(array) {|i|
+  collection>> i.upcase}
